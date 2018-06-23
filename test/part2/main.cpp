9c6a01a89a38855912f4a24767463cfac46c6fc5
Guy Waldman <guywald1@gmail.com>
2018-06-23 09:02:40 +0300

    Downgrade catch & remove vector initialization lists

--- test/part2/main.cpp
+++ test/part2/main.cpp
@@ -37,7 +37,8 @@ vector<T> derefVec(vector<T*> const& vec) {
 
 TEST_CASE("Collect") {
   // normal
-  auto vec = mapVecToPtrs(vector<int>{1, 2, 5, 3, 8, 1});
+  vector<int> vecRaw = {1, 2, 5, 3, 8, 1};
+  auto vec = mapVecToPtrs(vecRaw);
   REQUIRE(Stream<int>::of(vec).collect<vector<int*>>().size() == 6);
   REQUIRE(Stream<int>::of(vec).collect<set<int*>>().size() == 6);
 
@@ -58,12 +59,14 @@ TEST_CASE("Collect") {
     res.push_back(*val);
   }
   std::sort(res.begin(), res.end());
-  REQUIRE(res == vector<int>{1, 2, 3, 4});
+  vector<int> expected = {1, 2, 3, 4};
+  REQUIRE(res == expected);
 }
 
 // note: requires `Stream#count`
 TEST_CASE("filter") {
-  auto vec = mapVecToPtrs(vector<string>{"air", "cat", "cool", "block", "care"});
+  vector<string> vecRaw = {"air", "cat", "cool", "block", "care"};
+  auto vec = mapVecToPtrs(vecRaw);
   REQUIRE(Stream<string>::of(vec).filter([](string const* str) { return str->size() > 42; }).count() ==
           0);  // no matches
   REQUIRE(Stream<string>::of(vec).filter([](string const* str) { return str->size() < 42; }).count() ==
@@ -73,30 +76,36 @@ TEST_CASE("filter") {
 }
 
 TEST_CASE("map") {
-  auto vec = mapVecToPtrs(vector<string>{"a", "aa", "aaaa", "aaa"});
+  vector<string> vecRaw = {"a", "aa", "aaaa", "aaa"};
+  auto vec = mapVecToPtrs(vecRaw);
   auto sizesPtrs =
       Stream<string>::of(vec).map<int>([](string const* str) { return new int(str->size()); }).collect<vector<int*>>();
   auto sizes = derefVec(sizesPtrs);
   std::sort(sizes.begin(), sizes.end());
-  REQUIRE(sizes == vector<int>{1, 2, 3, 4});
+  vector<int> expected = {1, 2, 3, 4};
+  REQUIRE(sizes == expected);
 }
 
 TEST_CASE("distinct") {
-  vector<int*> vec = mapVecToPtrs(vector<int>{1, 2, 5, 3, 8, 1});
+  vector<int> vecRaw = {1, 2, 5, 3, 8, 1};
+  vector<int*> vec = mapVecToPtrs(vecRaw);
   REQUIRE(Stream<int>::of(vec).distinct().count() == 5);
   REQUIRE(Stream<int>::of(vec).distinct([](int const* n, int const* m) { return *n % 2 == *m % 2; }).count() == 2);
 }
 
 TEST_CASE("forEach") {
-  vector<int*> vec = mapVecToPtrs(vector<int>{1, 2, 5, 3, 8, 1});
+  vector<int> vecRaw = {1, 2, 5, 3, 8, 1};
+  vector<int*> vec = mapVecToPtrs(vecRaw);
   vector<int> res;
   Stream<int>::of(vec).forEach([&res](int* num) { res.push_back(*num); });
   std::sort(res.begin(), res.end());
-  REQUIRE(res == vector<int>{1, 1, 2, 3, 5, 8});
+  vector<int> expected = {1, 1, 2, 3, 5, 8};
+  REQUIRE(res == expected);
 }
 
 TEST_CASE("reduce") {
-  auto vec = mapVecToPtrs(vector<int>{1, 2, 5, 3, 8, 1});
+  vector<int> vecRaw = {1, 2, 5, 3, 8, 1};
+  vector<int*> vec = mapVecToPtrs(vecRaw);
   int initial = 0;
   int* sum =
       Stream<int>::of(vec).reduce(&initial, [](int const* acc, int const* curr) { return new int(*acc + *curr); });
@@ -104,15 +113,17 @@ TEST_CASE("reduce") {
 }
 
 TEST_CASE("max/min") {
-  auto vec = mapVecToPtrs(vector<int>{1, 2, 5, 3, 8, 1});
+  vector<int> vecRaw = {1, 2, 5, 3, 8, 1};
+  vector<int*> vec = mapVecToPtrs(vecRaw);
   REQUIRE(*Stream<int>::of(vec).max() == 8);
-  auto vec2 = mapVecToPtrs(vector<int>{42});
+  vector<int*> vec2 = {new int(42)};
   REQUIRE(*Stream<int>::of(vec2).min() == 42);
   REQUIRE(*Stream<int>::of(vec2).max() == 42);
 }
 
 TEST_CASE("allMatch & anyMatch") {
-  auto vec = mapVecToPtrs(vector<string>{"cat", "dog"});
+  vector<string> vecRaw = {"cat", "dog"};
+  vector<string*> vec = mapVecToPtrs(vecRaw);
   REQUIRE(Stream<string>::of(vec).allMatch([](string const* str) { return str->size() == 3; }) == true);
   REQUIRE(Stream<string>::of(vec).allMatch([](string const* str) { return str->size() == 2; }) == false);
   REQUIRE(Stream<string>::of(vec).allMatch([](string const* str) { return str->at(0) == 'c'; }) == false);
@@ -121,7 +132,8 @@ TEST_CASE("allMatch & anyMatch") {
 }
 
 TEST_CASE("findFirst") {
-  auto vec = mapVecToPtrs(vector<int>{1, 2, 3, 7, 6, 5, 4});
+  vector<int> vecRaw = {1, 2, 3, 7, 6, 5, 4};
+  vector<int*> vec = mapVecToPtrs(vecRaw);
   REQUIRE(Stream<int>::of(vec).findFirst([](int const* n) { return *n > 42; }) == nullptr);
   REQUIRE(*Stream<int>::of(vec).findFirst([](int const* n) { return *n < 2; }) == 1);
   REQUIRE(*Stream<int>::of(vec).findFirst([](int const* n) { return *n > 1; }) == 2);
@@ -130,30 +142,36 @@ TEST_CASE("findFirst") {
 }
 
 TEST_CASE("integration") {
-  auto vec = mapVecToPtrs(vector<int>{1, 2, 3, 7, 6, 5, 4, 2, 1});
+  vector<int> vecRaw = {1, 2, 3, 7, 6, 5, 4, 2, 1};
+  vector<int*> vec = mapVecToPtrs(vecRaw);
   REQUIRE(Stream<int>::of(vec)
               .filter([](int const* n) { return *n <= 4; })  // 1, 2, 3, 4, 2, 1
               .distinct()                                    // 1, 2, 3, 4
               .count() == 4);
 
-  auto vec2 = mapVecToPtrs(vector<string>{"Michael Scott", "Dwight Schrute", "Pam Beasley"});
+  vector<string> vecRaw2 = {"Michael Scott", "Dwight Schrute", "Pam Beasley"};
+  vector<string*> vec2 = mapVecToPtrs(vecRaw2);
+  vector<int> expected = {0, 1, 1};
   REQUIRE(derefVec(
               Stream<string>::of(vec2)
                   .map<int>([](string const* str) { return new int(str->size()); })  // 13, 14, 11
                   .map<int>([](int const* num) { return new int(*num % 2); })        // 1, 0, 1
                   .sorted()                                                          // 0, 1, 1
-                  .collect<vector<int*>>()) == vector<int>{0, 1, 1});
+                  .collect<vector<int*>>()) == expected);
 
-  auto vec3 = mapVecToPtrs(vector<string>{"Bears", "Beets", "Battlestar Galactica"});
+  vector<string> vecRaw3 = {"Bears", "Beets", "Battlestar Galactica"};
+  vector<string*> vec3 = mapVecToPtrs(vecRaw3);
+  vector<char> expected2 = {'a', 's'};
   REQUIRE(derefVec(
               Stream<string>::of(vec3)
                   .sorted([](string const* str1, string const* str2) { return str1->size() > str2->size(); })  // "Battlestar Galactica", "Bears", "Beats"
                   .map<char>([](string const* str) { return new char(str->at(str->size() - 1)); })             // 'a', 's', 's'
                   .distinct()                                                                                  // 'a', 's'
-                  .collect<vector<char*>>()) == vector<char>{'a', 's'});
+                  .collect<vector<char*>>()) == expected2);
 
   map<char, unsigned*> histo;
   auto vec4 = mapVecToPtrs(vector<char>{'h', 'e', 'l', 'l', 'o'});
+  vector<unsigned> expected3 = {1, 1, 1, 2};
   Stream<char>::of(vec4).forEach([&histo](char const* c) {
     auto it = histo.find(*c);
     if (it != histo.end()) {
@@ -163,5 +181,5 @@ TEST_CASE("integration") {
     }
   });
   auto values = derefVec(Stream<unsigned>::of(histo).sorted().collect<vector<unsigned*>>());
-  REQUIRE(values == vector<unsigned>{1, 1, 1, 2});
+  REQUIRE(values == expected3);
 }
